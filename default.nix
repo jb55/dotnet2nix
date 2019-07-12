6e79518341242d04886389c42de74b8b275b42dd
William Casarin <jb55@jb55.com>
2019-07-12 11:31:51 -0700

    easier nix build
    Signed-off-by: William Casarin <jb55@jb55.com>

--- default.nix
+++ default.nix
@@ -1,36 +1,42 @@
-{ stdenv, lib, bash, callPackage, writeText, makeWrapper, writeScript, dotnet-sdk,
-  patchelf, libunwind, coreclr, libuuid, curl, zlib, icu }:
+{pkgs ? import <nixpkgs> {}}:
 
-let
-  env = ''
-    tmp="$(mktemp -d)"
-    export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-    export DOTNET_CLI_TELEMETRY_OPTOUT=1
-    export HOME="$tmp"
-  '';
+let pkg = 
+  { stdenv, lib, bash, callPackage, writeText, makeWrapper, writeScript, dotnet-sdk,
+    patchelf, libunwind, coreclr, libuuid, curl, zlib, icu }:
 
-  dotnetBuildPhase = { path, target, pkgset, config, project }: ''
-    ${env}
-    pushd ${path}
-    dotnet publish -r ${target} --source ${pkgset} -c ${config} ${project}
-    popd
-  '';
+  let
+    env = ''
+      tmp="$(mktemp -d)"
+      export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+      export DOTNET_CLI_TELEMETRY_OPTOUT=1
+      export HOME="$tmp"
+    '';
 
-  nuget = callPackage ./nix/nuget.nix {};
-  dotnet = callPackage ./nix/dotnet.nix { inherit nuget coreclr dotnet-sdk; };
+    dotnetBuildPhase = { path, target, pkgset, config, project }: ''
+      ${env}
+      pushd ${path}
+      dotnet publish -r ${target} --source ${pkgset} -c ${config} ${project}
+      popd
+    '';
 
-in dotnet.mkDotNetCoreProject {
-  project = "dotnet2nix";
-  version = "1.0.0";
-  config = "Release";
+    nuget = callPackage ./nix/nuget.nix {};
+    dotnet = callPackage ./nix/dotnet.nix { inherit nuget coreclr dotnet-sdk; };
 
-  src = ./.;
+  in dotnet.mkDotNetCoreProject {
+    project = "dotnet2nix";
+    version = "1.0.0";
+    config = "Release";
 
-  meta = with stdenv.lib; {
-    description = "Generate nix expressions for dotnet core projects";
-    homepage = "https://github.com/jb55/dotnet2nix";
-    license = licenses.mit;
-    maintainers = [ maintainers.jb55 ];
-    platforms = with platforms; linux;
+    src = ./.;
+
+    meta = with stdenv.lib; {
+      description = "Generate nix expressions for dotnet core projects";
+      homepage = "https://github.com/jb55/dotnet2nix";
+      license = licenses.mit;
+      maintainers = [ maintainers.jb55 ];
+      platforms = with platforms; linux;
+    };
   };
-}
+
+in
+  pkgs.callPackage pkg { }
